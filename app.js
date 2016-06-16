c2d5dcc9fa122590c564ea613be382d31016a410
xm-71 <alexcannon2@live.com>
2016-06-16 10:44:47 -0400

    Add express & run basic server

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+  
+});
+
+app.listen(3030, function(){
+  console.log('Listening on http://localhost:3030...');
+});
