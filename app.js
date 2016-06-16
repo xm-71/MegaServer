f21b7b2faf21972ccf5c8e997b83d37284ffd48f
xm-71 <alexcannon2@live.com>
2016-06-16 10:58:44 -0400

    JSON code added & supervisor added  to stop crashing

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-  
+  res.json([
+    {
+      title: 'Hardcoded note',
+      body_html: ' My cool note.'
+    },
+    {
+      title:'Also Hardcoded',
+      body_html:'So sweet'
+    }
+  ]);
 });
 
 app.listen(3030, function(){
